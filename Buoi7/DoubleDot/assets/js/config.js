31632995d056a0a7db3075b100bcc55f076977cd
Lan <lannguyenphuc10a10123@gmail.com>
2024-06-16 14:46:55 +0700

    create page

---
+++ Buoi7/DoubleDot/assets/js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDamaWGBSMJm_NBapBxEdqWHdnxaKF0TTM",
+  authDomain: "doubledot2-fa14b.firebaseapp.com",
+  projectId: "doubledot2-fa14b",
+  storageBucket: "doubledot2-fa14b.appspot.com",
+  messagingSenderId: "214606635887",
+  appId: "1:214606635887:web:978d764cc20f5549ce3e9a",
+};
+
+firebase.initializeApp(firebaseConfig);
